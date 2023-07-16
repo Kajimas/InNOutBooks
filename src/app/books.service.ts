ef8018fec3403cde21742e54a0c6b02a603ace80
First Last <FL@academind.com>
2023-07-16 00:01:58 -0400

    Initial commit

---
+++ src/app/books.service.ts
@@ -0,0 +1,50 @@
+/*
+Title: books.service.ts
+Author: William Egge
+Date: 22 June 2023
+Description: The books service ts file for in-n-out-books app.
+*/
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IBook } from './book.interface';
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  isbns: Array<string> = [
+    '0345339681',
+    '0261103571',
+    '9780593099322',
+    '9780261102361',
+    '9780261102378',
+    '9780590302715',
+    '9780316769532',
+    '9780743273565',
+    '9780590405959',
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  getBooks() {
+    let params = new HttpParams();
+
+    params = params.append('bibkeys', `ISBN:${this.isbns.join(',')}`);
+    params = params.append('format', 'json');
+    params = params.append('jscmd', 'details');
+    return this.http.get('https://openlibrary.org/api/books', {params: params})
+  }
+
+  // getBook(isbn: string): IBook {
+  //   for (let book of this.books) {
+  //     if (book.isbn === isbn) {
+  //       return book;
+  //     }
+  //   }
+  // }
+}
